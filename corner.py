5ac1cd000bb96db68a22bc7f075c5a9ed43896de
trantuananh-123 <tom1810.2001@gmail.com>
2022-11-16 09:21:19 +0700

    update code

--- corner.py
+++ corner.py
@@ -15,7 +15,7 @@ def corner(path):
     ])
 
     # losses = model.fit(dataset, epochs=100, verbose=True, learning_rate=0.001)
-    model.load('D:/Nam-4/Nam-4/HTTM/Flask/yolov7/weights/id_card_4_corner.pth', ['top_left', 'top_right', 'bottom_right', 'bottom_left'])
+    model.load('./weights/corner_2.pth', ['top_left', 'top_right', 'bottom_right', 'bottom_left'])
 
     fname = path
     image = utils.read_image(fname)
@@ -26,14 +26,14 @@ def corner(path):
         boxes.numpy(), labels, 0.15)
 
 
-    for i, bbox in enumerate(boxes):
+    for i, bbox in enumerate(final_boxes):
         bbox = list(map(int, bbox))
         x_min, y_min, x_max, y_max = bbox
         cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
         cv2.putText(image, labels[i], (x_min, y_min),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
 
-        final_points = list(map(get_center_point, boxes))
+        final_points = list(map(get_center_point, final_boxes))
         label_boxes = dict(zip(final_labels, final_points))
 
     return image
